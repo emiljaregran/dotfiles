cd2fa29334ec1ec0ca03a2c284d9edc704fb77fb
Emil Jaregran <emil@jaregran.se>
2019-09-03 22:16:55 +0200

    Fixed symbols in status bar

--- i3/statusbar.py
+++ i3/statusbar.py
@@ -6,34 +6,38 @@ from i3pystatus import Status
 
 status = Status()
 
+# Unicode values of icons
+# https://fontawesome.com/cheatsheet
+
 status.register("clock",
-    format="  %a %-d %b %H:%M:%S",
+    format="\uf073 %a %-d %b %H:%M:%S",
     color='#61AEEE',
     interval=1,)
 
 status.register("pulseaudio",
     color_unmuted='#98C379',
     color_muted='#E06C75',
-    format_muted=' [muted]',
-    format=" {volume}%")
+    format_muted='\uf026 [muted]',
+    format="\uf028 {volume}%")
 
 status.register("temp",
     color='#78EAF2',
     format="\uf2c9 {temp:.0f} °C",)
 
 status.register("cpu_usage",
-    format=" {usage:02}%")
+    format="\uf0e4 {usage:02}%")
 
 status.register("network",
     interface="wlp3s0",
     color_up="#8AE234",
     color_down="#EF2929",
-    format_up=" {essid} ({quality:.0f}%)  {kbs} kbs",
-    format_down="",)
+    dynamic_color=False,
+    format_up="\uf1eb {essid} ({quality:.0f}%) \uf093 {bytes_sent} kB/s \uf019 {bytes_recv} kB/s",
+    format_down="\uf1eb Not connected",)
 
 status.register("backlight",
     interval=1,
-    format=" {percentage:.0f}%",
+    format="\uf042 {percentage:.0f}%",
     backlight="intel_backlight",)
 
 # Internal battery
@@ -49,7 +53,7 @@ status.register("battery"
     full_color="#D19A66",
     status={
         "DIS": "\uf241",
-        "CHR": "\uf240",
+        "CHR": "\uf1e6",
         "FULL": "\uf240",
     },)
 
@@ -66,7 +70,7 @@ status.register("battery"
     full_color="#D19A66",
     status={
         "DIS": "\uf241",
-        "CHR": "\uf240",
+        "CHR": "\uf1e6",
         "FULL": "\uf240",
     },)
 
@@ -74,17 +78,17 @@ status.register("mem",
     color="#999999",
     warn_color="#E5E500",
     alert_color="#FF1919",
-    format=" {avail_mem:.1f} GB",
+    format="\uf2db {avail_mem:.1f} GB",
     divisor=1073741824,)
 
 status.register("disk"
     ,color='#ABB2BF',
     path="/",
-    format="\uf07b {avail:.0f} GB",)
+    format="\uf0a0 {avail:.0f} GB",)
 
 status.register("disk",
     color='#56B6C2',
     path="/home",
-    format=" {avail:.0f} GB",)
+    format="\uf015 {avail:.0f} GB",)
 
 status.run()
