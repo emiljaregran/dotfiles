b5b75b4cf86b8e2b0081463ddb597381ea039515
Emil Jaregran <emil@jaregran.se>
2023-07-05 23:09:04 +0200

    nvim: Increase file tree max width

--- nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/lua/core/plugin_config/nvim-tree.lua
@@ -3,6 +3,13 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Setup using default options
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+    view = {
+        width = {
+            min = 30,
+            max = 50
+        }
+    }
+})
 
 nmap{'<leader>n', '<cmd>NvimTreeFindFileToggle<CR>'}
