fd3421596e4a695d1bcc183bfb9488c0fddae133
Emil Jaregran <emil@jaregran.se>
2026-04-16 06:45:33 +0200

    nvim: Configure nvim-tree widths

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -3,6 +3,12 @@ return {
   src = 'https://github.com/nvim-tree/nvim-tree.lua',
   setup = function()
     require('nvim-tree').setup({
+      view = {
+        width = {
+          min = 30,
+          max = 50,
+        }
+      },
       vim.keymap.set('n', '<leader>n', '<cmd>NvimTreeFindFileToggle<CR>')
     });
   end,
